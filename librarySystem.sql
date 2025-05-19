1df33058e2d12ee4eee9e0ef267d1cd86f6a881f
Shalama Fadilla <shalamafadillaa@gmail.com>
2025-05-19 09:48:32 +0700

    create a table for book
    create a new table for book, includes its attribute, such as bookID, bookName, author, and stock. Where bookID is the primary key and a constraint to check book's stock.

--- librarySystem.sql
+++ librarySystem.sql
@@ -5,3 +5,13 @@ create table student
     studentName varchar (100) not null,
     studentClass char (4) not null
 );
+
+-- create a table for book
+create table book
+(
+    bookID serial not null PRIMARY KEY,
+    bookName varchar (100) not null,
+    author varchar (100) not null,
+    stock int not null,
+    constraint check_book_stock check(stock >= 0)
+);
